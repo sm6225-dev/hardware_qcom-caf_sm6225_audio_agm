d3776180510e80f1d0ce6c890e087e1a106bd654
dianlujitao <dianlujitao@lineageos.org>
2025-03-25 14:41:22 +0800

    plugins: tinyalsa: fix build
    Change-Id: I4f8ef8bceb568b4102983d46b31eb424eabf6e8d
    Signed-off-by: Guidix <guidixm@gmail.com>

--- plugins/tinyalsa/src/agm_pcm_plugin.c
+++ plugins/tinyalsa/src/agm_pcm_plugin.c
@@ -172,7 +172,7 @@ static inline int snd_mask_val(const struct snd_mask *mask)
     return 0;
 }
 
-static unsigned int agm_format_to_bits(enum pcm_format format)
+static unsigned int agm_format_to_bits(enum agm_media_format format)
 {
     switch (format) {
     case AGM_FORMAT_PCM_S32_LE:
@@ -906,7 +906,7 @@ PCM_PLUGIN_OPEN_FN(agm_pcm_plugin)
     struct agm_media_config *media_config;
     struct agm_buffer_config *buffer_config;
     uint64_t handle;
-    enum agm_session_mode sess_mode = AGM_SESSION_DEFAULT;
+    int sess_mode = AGM_SESSION_DEFAULT;
     int ret = 0, session_id = device;
     void *card_node, *pcm_node;
 
